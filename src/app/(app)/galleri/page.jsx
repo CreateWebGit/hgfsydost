eb323615a8364952dff6813b5363464b676a47c8
CreateWebGit <hej@createweb.se>
2024-05-29 18:10:23 +0200

    first commit

--- src/app/(app)/galleri/page.jsx
+++ src/app/(app)/galleri/page.jsx
@@ -1,63 +1,7 @@
-import GoogleMaps from '@/app/components/GoogleMaps'
 import React from 'react'
 
-/*
-async function getData() {
-  const res = await fetch(
-    'https://maps.googleapis.com/maps/api/geocode/json?address=Huvudskarsvagen14&key=AIzaSyCnB2gPvNPFmo5PNRBWl1Is-IVSsMkxSm4',
-  )
-  // The return value is *not* serialized
-  // You can return Date, Map, Set, etc.
-
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error('Failed to fetch data')
-  }
-
-  const data = await res.json()
-  console.log('TEST: place response ', data)
-
-  return data
-}
-*/
-
-/*
-export async function getServerSideProps() {
-  const res = await fetch(
-    'https://maps.googleapis.com/maps/api/geocode/json?address=Huvudskärsvägen14&key=AIzaSyCnB2gPvNPFmo5PNRBWl1Is-IVSsMkxSm4',
-  )
-  const data = await res.json()
-
-  console.log(data)
-  console.log(data.results[0].formatted_address)
-  return {
-    props: { address: data.results[0].address_components[3].long_name },
-  }
-}
-*/
-
-const Galleri = async ({ users }) => {
-  // const data = await getData()
-
-  // console.log('haha', data.results)
-
-  return (
-    <div className="flex justify-center items-center h-screen">
-      hej
-      {/*<GoogleMaps />*/}
-    </div>
-  )
+const page = () => {
+  return <div>Galleri</div>
 }
 
-/*
-Galleri.getInitialProps = async () => {
-  const res = await fetch(
-    'https://maps.googleapis.com/maps/api/geocode/json?address=Huvudskärsvägen14&key=AIzaSyCnB2gPvNPFmo5PNRBWl1Is-IVSsMkxSm4',
-  )
-  const data = await res.json()
-  console.log('buu', data)
-  return { users: data }
-}
-*/
-
-export default Galleri
+export default page
